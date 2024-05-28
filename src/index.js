2a242c8af35e7492d6417ed5b3d086f07b42e615
Merveille Dohou <merveilledohou989@gmail.com>
2024-05-28 17:53:12 +0000

    deuxieme commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Finanfa from './finanfa';
-import App from './App'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
